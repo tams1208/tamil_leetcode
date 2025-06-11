aa78e945ad7deaa41e07571b7fda8e9ba66f9a31
tams1208 <tstamilselvankumarasamy@gmail.com>
2025-06-11 16:38:00 +0530

    Time: 1 ms (99.78%) | Memory: 41.9 MB (47.47%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -2,10 +2,10 @@ class Solution {
     public char findTheDifference(String s, String t) {
         char result = 0;
         for(char c : s.toCharArray()){
-            result ^= c;
+            result ^=c;
         }
         for(char c : t.toCharArray()){
-            result ^= c;
+            result ^=c;
         }
         return result;
     }
