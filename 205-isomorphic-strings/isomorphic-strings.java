c401fe382a9e2f85d0de9a7e5ea576ff46babab6
tams1208 <tstamilselvankumarasamy@gmail.com>
2025-06-11 16:11:37 +0530

    Time: 11 ms (68.13%) | Memory: 42.4 MB (84.85%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.HashSet;
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        HashMap<Character, Character> map = new HashMap<>();
+        HashSet<Character> mapped = new HashSet<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char sc = s.charAt(i); 
+            char tc = t.charAt(i); 
+
+            if (map.containsKey(sc)) {
+                if (map.get(sc) != tc) return false; 
+            } else {
+                if (mapped.contains(tc)) return false; 
+                map.put(sc, tc); 
+                mapped.add(tc);  
+            }
+        }
+
+        return true;
+    }
+}
